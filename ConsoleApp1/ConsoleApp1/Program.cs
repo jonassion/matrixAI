fe1821f2a631a321e8f9c4b92b693c8f3d49a5cd
tester <yangzhaohui@hs-defense.com>
2018-09-28 16:39:46 +0800

    upload console project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("helloworld");
+        }
+
+        public int Plus(int a, int b)
+        {
+            int result = 0;
+            int test = 0;
+            checked
+            {
+                result = a + b;
+            }
+            return result;
+        }
+
+        public int Minus(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1Tests/ProgramTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        Program p = new Program();
+
+        [TestMethod()]
+        public void PlusTest()
+        {
+            
+            Assert.AreEqual(p.Plus(1, 1), 2);
+        }
+
+        [TestMethod()]
+        public void MinusTest()
+        {
+            Assert.AreEqual(p.Minus(1, 1), 0);
+            Assert.AreEqual(p.Minus(100, 1), 99);
+        }
+    }
+}
